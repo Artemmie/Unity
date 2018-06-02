67382163f90559ada20d2cdff6283bd562e01943
Art <artemgerasimov55@gmail.com>
2018-06-03 00:24:03 +0300

    New game
    Poly terrain demo added
    Camera movement added
    Mob for each team added
    Moving pattern
    Teleport to right location in the end

---
+++ TowerDefenseDemo/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    public static Transform[] pointsBlue;
+    public static Transform[] pointsRed;
+    void Awake()
+    {
+        if(CompareTag("Waypoint Blue"))
+        {
+            pointsBlue = new Transform[transform.childCount];
+            for (int i = 0; i < pointsBlue.Length; i++)
+            {
+                pointsBlue[i] = transform.GetChild(i);
+            }
+        }
+        if (CompareTag("Waypoint Red"))
+        {
+            pointsRed = new Transform[transform.childCount];
+            for (int i = 0; i < pointsRed.Length; i++)
+            {
+                pointsRed[i] = transform.GetChild(i);
+            }
+        }
+    }
+}
