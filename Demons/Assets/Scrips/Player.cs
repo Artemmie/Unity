f05e24025d029e5c470d7c0bd948208393c7133e
Art <artemgerasimov55@gmail.com>
2019-05-15 16:32:01 +0300

    quick commit before reverting
    dadada

--- Demons/Assets/Scrips/Player.cs
+++ Demons/Assets/Scrips/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] PlayerAnimation pA;
 
     [SerializeField] float jump = 5.0f;
     [SerializeField] float move, speed = 3.0f;
@@ -14,6 +15,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        pA = GetComponent<PlayerAnimation>();
     }
 
     // Update is called once per frame
@@ -30,13 +32,15 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
+        Debug.Log("Jumping");
         if (Input.GetKeyDown("space") && Raycast())
-            rb.velocity = new Vector2(move, jump);
+            rb.velocity = new Vector2(move * speed, jump);
     }
 
     private bool Raycast()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, groundLayer.value);
+        Debug.DrawLine(transform.position, Vector2.down, Color.red, 30f);
         if (hit.collider != null)
             return true;
         return false;
