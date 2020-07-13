2d72cf7522995e7388ee768cd851455cb076bfc7
Art <artemgerasimov55@gmail.com>
2020-07-14 01:40:17 +0300

    Procedul Generated Dungeon
    wooh it was a hard day but I managed to make it happen!
    Floor is procedul generated.
    Dungeon is procedul generated.
    Fixed some bugs.

---
+++ DungeonGameV0.1/Assets/Scripts/DungeonGeneration.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGeneration : MonoBehaviour
+{
+
+    [SerializeField] private int numberOfRooms;
+    private Room[,] rooms;
+    private Room currentRoom;
+    private static DungeonGeneration instance = null;
+    private void Awake() 
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+            this.currentRoom = GenerateDungeon();
+        } 
+        else
+        {
+            string roomPrefabName = instance.currentRoom.PrefabName();
+            GameObject roomObject = (GameObject) Instantiate (Resources.Load(roomPrefabName));
+            Destroy(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        string roomPrefabName = this.currentRoom.PrefabName ();
+        GameObject roomObject = (GameObject)Instantiate(Resources.Load(roomPrefabName));
+        
+    }
+    private Room GenerateDungeon()
+    {  
+        int gridSize = 3 * numberOfRooms;
+        rooms = new Room[gridSize, gridSize];
+        Vector2Int initialRoomCoordinate = new Vector2Int ((gridSize /2) -1, (gridSize /2) - 1);
+        Queue<Room> roomsToCreate = new Queue<Room>();
+        roomsToCreate.Enqueue (new Room(initialRoomCoordinate.x, initialRoomCoordinate.y));
+        List<Room> createdRooms = new List<Room>();
+        while (roomsToCreate.Count > 0 && createdRooms.Count < numberOfRooms)
+        {
+            Room currentRoom = roomsToCreate.Dequeue();
+            this.rooms[currentRoom.roomCoordinate.x, currentRoom.roomCoordinate.y] = currentRoom;
+            createdRooms.Add(currentRoom);
+            AddNeighbors(currentRoom,roomsToCreate);
+        }
+        foreach (Room room in createdRooms)
+        {
+            List<Vector2Int> neighborCoordinates = room.NeighborCoordinates();
+            foreach (Vector2Int coordinate in neighborCoordinates)
+            {
+                Room neighbor = this.rooms[coordinate.x,coordinate.y];
+                if(neighbor != null)
+                {
+                    room.Connect(neighbor);
+                }
+            }
+        }
+        return this.rooms[initialRoomCoordinate.x,initialRoomCoordinate.y];
+
+    }
+    private void AddNeighbors(Room currentRoom, Queue<Room> roomsToCreate)
+    {
+        List<Vector2Int> neighborCoordinates = currentRoom.NeighborCoordinates();
+        List<Vector2Int> availaibleNeighbors = new List<Vector2Int>();
+        foreach (Vector2Int coordinate in neighborCoordinates)
+        {
+            if (this.rooms[coordinate.x,coordinate.y] == null)
+            {
+                availaibleNeighbors.Add(coordinate);
+            }
+        }
+        int numberofNeighbors = (int)Random.Range(1,availaibleNeighbors.Count);
+        for (int neighborIndex = 0; neighborIndex < numberofNeighbors; neighborIndex++)
+        {
+            float randomNumber = Random.value;
+            float roomFrac = 1f / (float)availaibleNeighbors.Count;
+            Vector2Int chosenNeighbor = new Vector2Int(0,0);
+            foreach (Vector2Int coordinate in availaibleNeighbors)
+            {
+                if (randomNumber < roomFrac)
+                {
+                    chosenNeighbor = coordinate;
+                    break;
+                }
+                else
+                {
+                    roomFrac += 1f / (float)availaibleNeighbors.Count;
+                }
+            }
+            roomsToCreate.Enqueue(new Room(chosenNeighbor));
+            availaibleNeighbors.Remove(chosenNeighbor);
+        }
+    }
+    private void PrintGrid() 
+    {
+        for (int rowIndex = 0; rowIndex < this.rooms.GetLength (1); rowIndex++) 
+        {
+            string row = "";
+            for (int columnIndex = 0; columnIndex < this.rooms.GetLength (0); columnIndex++) 
+            {
+                if (this.rooms [columnIndex, rowIndex] == null) 
+                {
+                    row += "X";
+                } 
+                else 
+                {
+                    row += "R";
+                }
+            }
+            Debug.Log (row);
+        }
+    }
+    public void MoveToRoom(Room room)
+    {
+        this.currentRoom = room;
+    }
+    public Room CurrentRoom()
+    {
+        return this.currentRoom;
+    }
+}
---
+++ DungeonGameV0.1/Assets/Scripts/EnterDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : MonoBehaviour
+{
+    [SerializeField] string direction;
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject dungeon = GameObject.FindGameObjectWithTag("Dungeon");
+            DungeonGeneration dungeonGeneration = dungeon.GetComponent<DungeonGeneration>();
+            Room room = dungeonGeneration.CurrentRoom();
+            dungeonGeneration.MoveToRoom(room.Neighbor(this.direction));
+            SceneManager.LoadScene("Level V1.0");
+        }
+    } 
+
+}
+
