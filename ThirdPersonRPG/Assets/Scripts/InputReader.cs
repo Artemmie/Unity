1fa3c95b469c09774ec6756683cded580ac99da9
Art <artemgerasimov55@gmail.com>
2022-06-30 08:09:41 +0300

    New character model, animations. Input Reader script
    Added a knight character model and specific animations to this model such as:
     - Idle
    - Walking
    - Running
    - Jumping
    
    * added changes to input reader and added jumping method.

--- ThirdPersonRPG/Assets/Scripts/InputReader.cs
+++ ThirdPersonRPG/Assets/Scripts/InputReader.cs
@@ -7,14 +7,13 @@ using UnityEngine.InputSystem;
 public class InputReader : MonoBehaviour, Controls.IPlayerActions
 {
     public event Action JumpEvent;
+    public event Action DodgeEvent;
     private Controls controls;
     private void Start()
     {
         controls = new Controls();
         controls.Player.SetCallbacks(this);
-
         controls.Player.Enable();
-
     }
     private void OnDestroy() 
     {
@@ -22,7 +21,12 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (!context.performed) {return;}
         JumpEvent?.Invoke();
+    }
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        if (!context.performed) {return;}
+        DodgeEvent?.Invoke();
     }
 }
--- ThirdPersonRPG/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ ThirdPersonRPG/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer = 0f;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) {}
 
     public override void Enter()
     {
         Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
         Debug.Log(timer);
-        if (timer <= 0f) stateMachine.SwitchState(new PlayerTestState(stateMachine));
+       // if (timer <= 0f) stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
     public override void Exit()
     {
         Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        timer = 0f;
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
